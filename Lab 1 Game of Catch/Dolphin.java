4cc7358df6cda48f27467eb3805bbb50bb45164a
riyadeokar <isaprocess666>
2018-10-01 09:25:20 -0700

    Called all the methods I wrote last time with act and changed my Greenfoot.getRandomNumber's with Math.random's

--- Lab 1 Game of Catch/Dolphin.java
+++ Lab 1 Game of Catch/Dolphin.java
@@ -12,22 +12,39 @@ public class Dolphin extends GameCharacter
      * Act - do whatever the Dolphin wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+
+    public void act()
+    {
+        moveAround(); 
+        turnAtEdge();
+        randomTurn();
+        lookForCatch();     
+    }
+
     public void moveAround() // allows object to move across the screen 
     {
-        move (10); 
+        move (5); 
     }    
 
     public void turnAtEdge() // allows object to turn if it reaches the edge of the world so it does not get stuck
     {
-        turn ((int) (Math.random()*10)); 
+        turn ((int)(Math.random()*5)); 
     }
 
     public void randomTurn() // allows object to turn 10% of the time in a randomized path (left and right) 
     {
+        if(Math.random()<=0.1)
+        {
+            turn((int)(Math.random()*5)); 
+        }
     }
 
     public void lookForCatch() // allows object to eat character to catch if main game character has collided with it 
     {
+        if(isTouching(Fish.class))
+        {
+            eat(Fish.class);
+        }
     }
 }
 
