bd7baa422a32e307f3c6b35d065b1cf13047c90a
riyadeokar <isaprocess666>
2018-11-19 09:23:47 -0800

    In-class Notes

---
+++ Unit 3/InheritancePractice/Animal.java
@@ -0,0 +1,15 @@
+public abstract class Animal 
+{
+    //fields
+    
+    //constructors
+    
+    //methods
+    public void sleep()
+    {
+        System.out.println("I am sleeping..zzzzzzz"); 
+    }
+    //abstract method 
+    //has no code -- all it is, is a method signature 
+    public abstract void eat(); 
+}
---
+++ Unit 3/InheritancePractice/Dog.java
@@ -0,0 +1,12 @@
+public class Dog extends Animal 
+{
+    //fields
+    
+    //constrcutors
+    
+    //methods
+    public void eat()
+    {
+        System.out.println("I love to eat meat!"); 
+    }
+}
