677336e6aae5feea9b08a068aa4f4c6d5bacf522
riyadeokar <isaprocess666>
2018-11-28 09:25:23 -0800

    in class lecture

---
+++ Unit 3/InheritancePractice/Pet.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of interface Pet here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public interface Pet
+{
+    public abstract void cuddle();
+    public abstract void playWithOwner(); 
+}
