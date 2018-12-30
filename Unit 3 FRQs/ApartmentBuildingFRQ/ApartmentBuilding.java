bff6b6c8fe42f7bbee4c6b139474e077e6eb7972
riyadeokar <isaprocess666>
2018-12-29 23:09:12 -0800

    Employee FRQ

---
+++ Unit 3 FRQs/ApartmentBuildingFRQ/ApartmentBuilding.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class ApartmentBuilding here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ApartmentBuilding extends Building 
+{
+    // instance variables - replace the example below with your own
+    private double[] aptWattHours;
+    private String adressApartment; 
+    private int numberApartments; 
+
+    /**
+     * Constructor for objects of class ApartmentBuilding
+     */
+    public ApartmentBuilding(String ad, int number)
+    {
+        super(ad);  
+        numberApartments = number; 
+        double[] aptWattHours = new double[numberApartments]; 
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public double amtOwed( )
+    {
+        
+    }
+}
